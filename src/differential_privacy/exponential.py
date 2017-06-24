0dd2d20eda4e4821fa3b6262854cd12052bdaed6
mbenlioglu <mbenlioglu@sabanciuniv.edu>
2017-06-24 23:47:28 +0300

    + Added necessary parameters for function call

--- src/differential_privacy/exponential.py
+++ src/differential_privacy/exponential.py
@@ -2,13 +2,15 @@ from src import db
 from src import utility as util
 
 k_neighbours = 5
+sensitivity = 1
+epsilon = 0.1
 movie_ids = [1]
 
 
 def main():
     for movie_id in movie_ids:
         movies = db.get_similarities_by_movie(movie_id, k_neighbours)
-        new_order = util.get_exponential_recommendation_order(movies)
+        new_order = util.get_exponential_recommendation_order(movies, epsilon, sensitivity)
         print movies, new_order
 
 
