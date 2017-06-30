bfc2d1173c5d97482fb2220c22d0f757bd3cf7e3
Mert <mertkosan@sabanciuniv.edu>
2017-06-30 15:20:48 +0300

    + exponentialMechanism  + init

---
+++ src/differential_privacy/exponentialMechanism.py
@@ -0,0 +1,17 @@
+def preprocessing():
+    pass
+
+
+def sampling():
+    pass
+
+
+def perturbation():
+    pass
+
+
+def get_exponential_recommendation_order():
+    preprocessing()
+    sampling()
+    perturbation()
+    pass
